dd86c14ef7656b7dd02ec4ff7c81acefa88a6cdf
rate Sai <1417604287@qq.com>
2023-09-26 18:01:36 +0800

    初始commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FoLi
+
+Developed with Unreal Engine 5
---
+++ Source/FoLi/FoLiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FoLiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FOLI_API AFoLiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
